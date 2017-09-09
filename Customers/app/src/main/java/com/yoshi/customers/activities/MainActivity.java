107dc1967346ec4e7f8224f249c2f8957a67d0b5
u201021101 <u201021101@upc.edu.pe>
2017-09-09 08:13:44 -0500

    Bottom navigation

---
+++ Customers/app/src/main/java/com/yoshi/customers/activities/MainActivity.java
@@ -0,0 +1,75 @@
+package com.yoshi.customers.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.yoshi.customers.R;
+import com.yoshi.customers.fragments.DateFragment;
+import com.yoshi.customers.fragments.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation=(BottomNavigationView)findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            return navigateAccordingTo(item.getItemId());
+        }
+    };
+
+    private boolean navigateAccordingTo(int id) {
+        try {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content, getFragmentFor(id))
+                    .commit();
+            return true;
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private Fragment getFragmentFor(int id) {
+        switch (id) {
+            case R.id.navigation_map:
+                return new MapFragment();
+            case R.id.navigation_date:
+                return new DateFragment();
+        }
+
+        return null;
+    }
+}
